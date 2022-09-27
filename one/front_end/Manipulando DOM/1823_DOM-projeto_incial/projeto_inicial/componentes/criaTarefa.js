ee853019fd76b83332d25fc5f838751c6ab2a34a
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2022-09-26 21:05:13 -0300

    [ADD] manipulando DOM

---
+++ one/front_end/Manipulando DOM/1823_DOM-projeto_incial/projeto_inicial/componentes/criaTarefa.js
@@ -0,0 +1,55 @@
+import { carregaTarefa } from './carregaTarefa.js'
+import BotaoConclui from './concluiTarefa.js'
+import BotaoDeleta from './deletaTarefa.js'
+ 
+
+
+export const handleNovoItem=(evento)=>{
+    evento.preventDefault() /*metodo que previne o comportamento padrão de um formulario */
+
+    const tarefas= JSON.parse(localStorage.getItem('tarefas')) || []
+
+    
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value /*pega o valor inserido no input, devolvendo o valor inserido no formulario*/
+
+    const calendario = document.querySelector('[data-form-date')
+    const data = moment(calendario.value)
+    const horario = data.format('HH:mm')
+
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const concluida =false
+
+    const dados={
+        valor,
+        dataFormatada,
+        horario,
+        concluida
+    }
+
+    const tarefasAtualizadas = [...tarefas, dados]
+
+
+    localStorage.setItem("tarefas",JSON.stringify(tarefasAtualizadas) )
+    input.value = " "
+    carregaTarefa()      
+}
+
+export const Tarefa = ({valor,horario,concluida},id) => { 
+
+    const tarefa = document.createElement('li')
+    
+    const conteudo= `<p class="content">${horario} * ${valor}</p>`
+    if(concluida){
+        tarefa.classList.add('done')
+    }
+    tarefa.classList.add('task')
+
+    tarefa.innerHTML=conteudo
+    tarefa.appendChild(BotaoConclui(carregaTarefa,id))
+    tarefa.appendChild(BotaoDeleta(carregaTarefa,id))
+   
+    return tarefa
+
+}
